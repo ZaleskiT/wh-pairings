9cd861042fc3823c77fcd1a646a17359192c6a8e
ZaleskiT <zaleskitimothy@yahoo.com>
2024-11-12 23:17:00 +0900

    refactor after a year

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OutcomesInputComponent } from './outcomes-input/outcomes-input.component';
-import { PairingsDisplayComponent } from './pairings-display/pairings-display.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/input', pathMatch: 'full' }, // Default route
-  { path: 'input', component: OutcomesInputComponent },
-  { path: 'pairings', component: PairingsDisplayComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OutcomesInputComponent } from './outcomes-input/outcomes-input.component';
-import { PairingsDisplayComponent } from './pairings-display/pairings-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OutcomesInputComponent,
-    PairingsDisplayComponent
+    OutcomesInputComponent
   ],
   imports: [
     BrowserModule,
